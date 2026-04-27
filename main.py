4495b541ab3ce30f1a346cd8794f7a4feeb1c8a0
Kayla Smith <ksmit649@students.kennesaw.edu>
2026-04-26 20:26:20 -0400

    Input information
    Create input for name, gifts, dates

---
+++ main.py
@@ -0,0 +1,18 @@
+# Create Lists
+names = []
+dates = []
+gifts = []
+event = []
+
+# Ask user for input
+name_input = input("Enter Full Name: ")
+date_input = input("Enter Date (MM/DD/YY): ")
+gift_input = input("Enter Gift: ")
+event_input = input("Type (Birthday/Anniversary/One-Time)")
+
+# Add new input to list
+names.append(name_input)
+dates.append(date_input)
+gifts.append(gift_input)
+event.append(event_input)
+
